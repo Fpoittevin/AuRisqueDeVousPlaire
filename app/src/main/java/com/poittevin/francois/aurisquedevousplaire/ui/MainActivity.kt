c161e2bf414dfa5f1ee3990d52ac24307cf9423e
Fpoittevin <dev@francoispoittevin.Fr>
2021-01-06 11:32:23 +0100

    gradle and packages configuration

--- app/src/main/java/com/poittevin/francois/aurisquedevousplaire/ui/MainActivity.kt
+++ app/src/main/java/com/poittevin/francois/aurisquedevousplaire/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.poittevin.francois.aurisquedevousplaire
+package com.poittevin.francois.aurisquedevousplaire.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.poittevin.francois.aurisquedevousplaire.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
