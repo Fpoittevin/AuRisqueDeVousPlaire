6640ee70ed610f651cb049cd9f2a00b1664485b3
Fpoittevin <dev@francoispoittevin.Fr>
2021-01-06 11:38:57 +0100

    configure main activity layout and toolbar menu

--- app/src/main/java/com/poittevin/francois/aurisquedevousplaire/ui/MainActivity.kt
+++ app/src/main/java/com/poittevin/francois/aurisquedevousplaire/ui/MainActivity.kt
@@ -2,11 +2,36 @@ package com.poittevin.francois.aurisquedevousplaire.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
 import com.poittevin.francois.aurisquedevousplaire.R
+import com.poittevin.francois.aurisquedevousplaire.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        configureToolbar()
     }
+
+    private fun configureToolbar() {
+        setSupportActionBar(binding.activityMainToolbar)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.activity_main_toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    private fun displayFragment(layoutId: Int, fragment: Fragment) {
+        supportFragmentManager
+            .beginTransaction()
+            .replace(layoutId, fragment)
+            .commit()
+    }
+
 }
